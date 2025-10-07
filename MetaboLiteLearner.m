8b677d7793d5fc8218b9727e4fd21db1a2d3a217
Joao Xavier <joaobxavier@gmail.com>
2025-10-07 10:01:08 -0400

    Update MetaboLiteLearner.m

--- MetaboLiteLearner.m
+++ MetaboLiteLearner.m
@@ -5,7 +5,7 @@ classdef MetaboLiteLearner
     properties
         % input properties
         xFullData; % the spectra for each metabolite
-        yFullData; % the reponse variable
+        yFullData; % the response variable
         kfold; % the kfold used to estimate loss and optimize components
         maxn; % the maximum number of components tested
         nrandomized; % the number of shuffiling iterations used to test
